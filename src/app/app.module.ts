0e55c916bfa29b37ac0f8298a5f9fd9768dad223
MeenaS <smeena.ece13@gmail.com>
2018-11-28 09:43:02 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FlotComponent } from './flot/flot.component';
+import { MorrisComponent } from './morris/morris.component';
+import { TablesComponent } from './tables/tables.component';
+import { ProductsComponent } from './products/products.component';
+import { OrdersComponent } from './orders/orders.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    SidebarComponent,
+    DashboardComponent,
+    FlotComponent,
+    MorrisComponent,
+    TablesComponent,
+    ProductsComponent,
+    OrdersComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
